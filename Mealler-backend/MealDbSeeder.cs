221b974bbe9d8be131b0431ab9e10db284bd0446
Oskar Samborowski <94199123+Haz-art@users.noreply.github.com>
2023-10-14 18:35:11 +0200

    new updated seeder

--- Mealler-backend/MealDbSeeder.cs
+++ Mealler-backend/MealDbSeeder.cs
@@ -1,33 +1,76 @@
 ﻿using Mealler_backend;
 
-public static class MealDbSeeder
+public class MealDbSeeder
 {
-    public static void Seed(IServiceProvider serviceProvider)
+    private readonly MealDbContext _context;
+
+    public MealDbSeeder(MealDbContext context)
     {
-        using (var scope = serviceProvider.CreateScope())
+        _context = context;
+    }
+
+    public void Seed()
+    {
+        SeedCategories();
+        SeedMeals();
+    }
+
+    private void SeedCategories()
+    {
+        if (!_context.Categories.Any())
         {
-            var dbContext = scope.ServiceProvider.GetRequiredService<MealDbContext>();
-
-            // Access the Meals DbSet
-            var meals = dbContext.Meals;
-
-            // Check if there are already meals in the database
-            if (!meals.Any())
+            var categories = new List<Category>
             {
-                // Seed the database with initial data
-                for (int i = 1; i <= 60; i++)
+                new Category { Name = "Pizza" },
+                new Category { Name = "Burgers" },
+                new Category { Name = "Sushi" },
+                new Category { Name = "Mexican" },
+                new Category { Name = "Italian" },
+                new Category { Name = "Chinese" },
+                new Category { Name = "Indian" },
+                new Category { Name = "Mediterranean" },
+                new Category { Name = "Japanese" },
+            };
+
+            _context.Categories.AddRange(categories);
+            _context.SaveChanges();
+        }
+    }
+
+    private void SeedMeals()
+    {
+        if (!_context.Meals.Any())
+        {
+            // Add at least 90 meals
+            var random = new Random();
+            var categories = _context.Categories.ToList();
+
+            var meals = new List<Meal>();
+            for (int i = 1; i <= 90; i++)
+            {
+                var meal = new Meal
                 {
-                    var meal = new Meal
-                    {
-                        Name = $"Meal {i}",
-                        // Set other properties as needed
-                    };
+                    Name = $"Meal {i}",
+                    Rating = random.Next(1, 6),
+                    Image = "meal_image_url", // Set your image URL
+                    Description = "Meal description",
+                };
 
-                    meals.Add(meal);
-                }
+                // Assign random categories to meals
+                var selectedCategories = categories
+                    .OrderBy(c => Guid.NewGuid()) // Shuffle the list of categories
+                    .Take(1)//.Take(random.Next(1, 4)) // Randomly select 1-3 categories
+                    .ToList();
 
-                dbContext.SaveChanges();
+                meal.MealCategories = selectedCategories
+                    .Select(c => new MealCategory { CategoryId = c.Id })
+                    .ToList();
+
+                meals.Add(meal);
             }
+
+            _context.Meals.AddRange(meals);
+            _context.SaveChanges();
         }
     }
 }
--- Mealler-backend/Program.cs
+++ Mealler-backend/Program.cs
@@ -9,16 +9,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<MealDbSeeder>();
 
 var app = builder.Build();
-
-MealDbSeeder.Seed(app.Services);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<MealDbSeeder>();
+    seeder.Seed();
 }
 
 app.UseHttpsRedirection();
