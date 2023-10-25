b938487100eff1d171611b8fc9a3638a1f0ca83c
Oskar Samborowski <94199123+Haz-art@users.noreply.github.com>
2023-10-25 18:28:10 +0200

    new dto, Post method for creating new meals

--- Mealler-backend/Controllers/MealController.cs
+++ Mealler-backend/Controllers/MealController.cs
@@ -13,6 +13,29 @@ public class MealController : ControllerBase {
         _context = context;
     }
 
+    [HttpPost("meal")]
+    public async Task<IActionResult> CreateMeal([FromBody] CreateMealInputModel inputModel)
+    {
+        if (inputModel == null)
+        {
+            return BadRequest("Invalid input data");
+        }
+
+        var newMeal = new Meal
+        {
+            Rating = inputModel.Rating,
+            Name = inputModel.Name,
+            Image = inputModel.Image,
+            Description = inputModel.Description,
+        };
+
+        _context.Meals.Add(newMeal);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetMealsAfterId), new { startId = newMeal.Id, count = 1 }, newMeal);
+    }
+
+
     [HttpGet("meal")]
     public async Task<IActionResult> GetMealsAfterId([FromQuery] int startId, [FromQuery] int count)
     {
---
+++ Mealler-backend/CreateMealInputModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Mealler_backend;
+public class CreateMealInputModel
+{
+    public float Rating { get; set; }
+    public string Name { get; set; }
+    public string Image { get; set; }
+    public string Description { get; set; }
+}
